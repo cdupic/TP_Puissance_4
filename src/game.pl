c9f26ce6d5ce085d6f46202b354c6d07f737a3fb
cdupic <dupicclement@gmail.com>
2025-12-02 11:22:21 +0100

    end game logic (for rows for now)

--- src/game.pl
+++ src/game.pl
@@ -1,4 +1,5 @@
 :- dynamic board/1.     % permet l'assertion et le retrait de faits board/1
+:- dynamic game_number/1.
 
 % Affiche le plateau (42 cases = 6 lignes × 7 colonnes)
 % 0-1-2-3-4-5-6
@@ -35,6 +36,7 @@ dropPiece(Board, Column, Move) :-
     Move is 35 + Column - 7*Row,
     nth0(Move, Board, Val),
     var(Val).
+
 playMove(Board,Move,NewBoard,Player) :-
 		copy_term(Board, NewBoard),
     	nth0(Move, NewBoard,Player).
@@ -44,6 +46,54 @@ applyIt(Board, NewBoard) :-
     retract(board(Board)), % on oublie l'ancienne règle qui n'est plus vraie
     assert(board(NewBoard)).
 
+
+linecompleted(Board, Move) :-
+    retractall(game_number(_)),      % compteur à 0 au début
+    assert(game_number(0)),
+
+    nth0(Move, Board, Player),
+    format("[linecompleted] Move=~w Player=~w~n", [Move, Player]),
+
+    Row is Move // 7,
+    format("  Row(computed)=~w~n", [Row]),
+
+    between(0, 6, Column),
+
+    Cell is Row*7 + Column,
+    format("  -> Column=~w CellIndex=~w~n", [Column, Cell]),
+
+    nth0(Cell, Board, Val),
+    format("     CellVal=~w (vs Player=~w)~n", [Val, Player]),
+
+    ( Val == Player -> incr ; reset_counter ),
+
+    game_number(N),
+    format("  game_number(after update)=~w~n", [N]),
+    N >= 4, !.                       % FIN
+
+
+incr :-
+    retract(game_number(N)),
+    N1 is N + 1,
+    assert(game_number(N1)).
+
+
+reset_counter :-
+    retract(game_number(_)),
+    assert(game_number(0)).
+
+
+gameOver(Board, Move) :-   % first check if board is full
+    write("Draw\n"),
+    between(0, 41, X),
+    nth0(X, Board, Val),
+    nonvar(Val).
+
+gameOver(Board, Move) :- % else check if move is winning move
+    % check if a line as been completed
+    linecompleted(Board, Move);
+    % check if a column has been completed
+    % check if a diagonal has been completed
 
 play(Player):-  write('New turn for:'), writeln(Player),
     		board(Board), % instanciate the board from the knowledge base
@@ -56,5 +106,5 @@ play(Player):-  write('New turn for:'), writeln(Player),
 
 init :-
     length(Board, 42),
-    assert(board(Board)),
-    play('X').
+    assert(board(Board)).
+    %play('X').
