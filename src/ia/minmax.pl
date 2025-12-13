2b741b390008522d2fce657c1950b550ba8694c3
Clement Dupic <clement.dupic@insa-lyon.fr>
2025-12-13 19:04:55 +0100

    feat(minmax) : enhancing the way points are counted

--- src/ia/minmax.pl
+++ src/ia/minmax.pl
@@ -4,12 +4,193 @@ minmaxIa(Board, Move, Player) :- % TODO: code the IA
     dropPiece(Board, Column, Move).
 
 
+get3AlignedInRow(Board, Move, Player, Count) :-
+    getRow(Board, Move, RowList),
+    write(RowList),
+    countTriples(RowList, Player, Count).
+
+get3AlignedInColumn(Board, Move, Player, Count) :-
+    getColumn(Board, Move, ColumnList),
+    countTriples(ColumnList, Player, Count).
+
+
+get3AlignedInDiagonalDownRight(Board, Move, Player, Count) :-
+    getDiagonalDownRight(Board, Move, DiagonalList),
+        write(DiagonalList),
+    countTriples(DiagonalList, Player, Count).
+
+get3AlignedInDiagonalDownLeft(Board, Move, Player, Count) :-
+    getDiagonalDownLeft(Board, Move, DiagonalList),
+    write(DiagonalList),
+    countTriples(DiagonalList, Player, Count).
+
+
+
+get2AlignedInRow(Board, Move, Player, Count) :-
+    getRow(Board, Move, RowList),
+    countTuples(RowList, Player, Count).
+
+
+get2AlignedInColumn(Board, Move, Player, Count) :-
+    getColumn(Board, Move, ColumnList),
+    countTuples(ColumnList, Player, Count).
+
+
+get2AlignedInDiagonalDownRight(Board, Move, Player, Count) :-
+    getDiagonalDownRight(Board, Move, DiagonalList),
+    countTuples(DiagonalList, Player, Count).
+
+get2AlignedInDiagonalDownLeft(Board, Move, Player, Count) :-
+    getDiagonalDownLeft(Board, Move, DiagonalList),
+    countTuples(DiagonalList, Player, Count).
+
+
+
+% Renvoie la valeur de la case Index dans Board ; remplace une variable par 'E'
+cell_value(Board, Index, ValOut) :-
+    nth0(Index, Board, Val),
+    ( var(Val) -> ValOut = 'E' ; ValOut = Val ).
+
+getColumn(Board, Move, ColumnList) :-
+    Col is Move mod 7,
+    findall(Val,
+            ( between(0,5,Row),
+              Index is Row*7 + Col,
+              cell_value(Board, Index, Val)
+            ),
+            ColumnList).
+
+getRow(Board, Move, RowList) :-
+    Row is Move // 7,
+    Start is Row * 7,
+    findall(Val,
+            ( between(0,6,Off),
+              Index is Start + Off,
+              cell_value(Board, Index, Val)
+            ),
+            RowList).
+
+getDiagonalDownRight(Board, Move, DiagonalList) :-
+    getTopLeft(Move, Start),
+    findall(Val,
+            ( between(0,5,Step),
+              Cell is Start + 8*Step,
+              Cell =< 41,
+              Col is Cell mod 7,
+              Col \= 6,
+              cell_value(Board, Cell, Val)
+            ),
+            DiagonalList).
+
+getDiagonalDownLeft(Board, Move, DiagonalList) :-
+    getTopRight(Move, Start),
+    findall(Val,
+            ( between(0,5,Step),
+              Cell is Start + 6*Step,
+              Cell =< 41,
+              Col is Cell mod 7,
+              Col \= 0,
+              cell_value(Board, Cell, Val)
+            ),
+            DiagonalList).
+
+% ------------------- TRIPLES -----------------------
+free_cell(Val) :-
+    Val == 'E'.
+
+countTriplesNew(List, Player, Acc, Count) :-
+    append(Prefix, [A,B,C|Suffix], List), % le append découpe suivant toutes les possibilités donc on teste bien toous les A,B,C possibles
+    A == Player, B == Player, C == Player,
+    write(Prefix), writeln(""), write([A,B,C|Suffix]), writeln(""),
+    % Succeeds when Prev is the last element of Prefix -> on regarde si la dernière case du préfix est libre
+    (( Prefix \= [], last(Prefix, Prev), free_cell(Prev) )
+    ;  % sinon on regarde si la première case de suffix est libre
+    ( Suffix \= [], Suffix = [Next|_], free_cell(Next) )),
+    !, % empêche de retrouver le meme triple
+    Acc1 is Acc + 1,
+    countTriplesNew(Suffix, Player, Acc1, Count).
+
+countTriplesNew(_, _, Count, Count).
+
+countTriples(List, Player, Count) :-
+    countTriplesNew(List, Player, 0, Count).
+
+
+
+
+% ------------------- TUPLES -----------------------
+
+
+
+countTuples(List, Player, Count) :-
+    countTuplesAux(List, Player, 0, Count).
+
+countTuplesAux(List, Player, Acc, Count) :-
+    append(Prefix, [A,B|Suffix], List),
+    A == Player,
+    B == Player,
+
+    free_left(Prefix, L),
+    free_right(Suffix, R),
+
+    L + R >= 2,
+
+    !,
+    Acc1 is Acc + 1,
+    countTuplesAux(Suffix, Player, Acc1, Count).
+
+countTuplesAux(_, _, Count, Count).
+
+free_left(Prefix, Count) :-
+    reverse(Prefix, Rev), % on inverse la liste dans prefix
+    free_run(Rev, Count).
+
+free_right(Suffix, Count) :-
+    free_run(Suffix, Count).
+
+free_run([], 0).
+free_run(['E'|Rest], Count) :-
+    free_run(Rest, C1),
+    Count is C1 + 1.
+
+free_run([H|_], 0) :-
+    H \= 'E'.
+% -----------------------------------------------------------------
+
+getWeightPosition(Board, Move, Player, Weight) :-
+    % On supppose que le coup a été simulé avant (j'ai pas réussi à le simuler en phase de test)
+    % on considère égalements toutes les positions sur les lignes/colonnes et diagonnales associées au move,
+    % pas seulement celles qui sont adjacentes au ooint qui vient d'être joué.
+
+    ( gameOver(Board, Move, _) ->
+        Weight = 100000
+    ;
+
+        get3AlignedInRow(Board, Move, Player, C1R),
+        get3AlignedInColumn(Board, Move, Player, C1C),
+        get3AlignedInDiagonalDownRight(Board, Move, Player, C1DR),
+        get3AlignedInDiagonalDownLeft(Board, Move, Player, C1DL),
+        Score3 is (C1R + C1C + C1DR + C1DL) * 100,
+
+        ( Score3 > 0 ->
+            Weight = Score3
+        ;
+            get2AlignedInRow(Board, Move, Player, C2R),
+            get2AlignedInColumn(Board, Move, Player, C2C),
+            get2AlignedInDiagonalDownRight(Board, Move, Player, C2DR),
+            get2AlignedInDiagonalDownLeft(Board, Move, Player, C2DL),
+            Score2 is (C2R + C2C + C2DR + C2DL) * 10,
+            Weight = Score2
+        )
+    ), !.
+
+
 max(X1,X2,Max) :-
     X1 > X2 -> Max = X1;
     Max = X2.
 
 min(X1,X2,Min) :-
-    X1 > X2 -> Max = X2;
+    X1 > X2 -> Min = X2;
     Min = X1.
 
 minimax(Depth, NodeIndex, _IsMax, Scores, H, OptimalValue) :-
@@ -37,4 +218,4 @@ logBase2(N,R) :-
                 N>1,
                 N1 is N//2,
                 logBase2(N1,R1),
-                R is R1 +1.
+                R is R1 + 1.
--- src/test.pl
+++ src/test.pl
@@ -1,6 +1,276 @@
 :- begin_tests(test_suite_name).
 
 test(getDiagonalDownRight) :-
+    Board = [
+      _,_,_,_,_,_,_,
+      _,_,_,_,_,_,'X',
+      _, 'X',_,_,_, 'X','X',
+      'X',_, 'X',_, 'X',_,_,
+      _, 'X',_, 'X',_,_, 'X',
+      'X','X','Y','X','X','X','X'
+    ],
+    getDiagonalDownRight(Board, 39, Diagonal),
+    assertion(Diagonal == ['E', 'X','X','X','X']).
+
+
+test(getDiagonalDownLeft) :-
+    Board = [
+      _,_,_,_,_,_,_,
+      _,_,_,_,_,_,'X',
+      _, 'X',_,_,_, 'X','X',
+      'X',_, 'X',_, 'X',_,_,
+      _, 'X',_, 'X',_,_, 'X',
+      'X','X','Y','X','X','X','X'
+    ],
+    getDiagonalDownLeft(Board, 39, Diagonal),
+    assertion(Diagonal == ['E', 'E', 'X']).
+
+:- end_tests(diagonal).
+
+:- begin_tests(triplesRow).
+
+test(get3AlignedInRow) :-
+    Board = [
+           _,_,_,_,_,_,_,
+           _,_,_,_,_,_,'X',
+           _  ,_  ,  _,  _,  _,  _,'X',
+           'X',_  ,'X',_  ,'X', _ , _ ,
+           _  ,'X',_  ,'X',_  ,_  ,'X',
+           'X','X','Y',_,'X','X','X'
+         ],
+    get3AlignedInRow(Board, 39, 'X', C1R),
+    assertion(C1R == 1).
+
+:- end_tests(triplesRow).
+
+:- begin_tests(triplesDiagonal).
+
+test(get3AlignedInDiagonalDownRight) :-
+    Board = [
+               _,_,_,_,_,_,_,
+               _,_,_,_,_,_,'X',
+               _  ,_  ,  _,  _,  _,  _,'X',
+               'X',_  ,'X',_  ,'X', _, _ ,
+               _  ,'X',_  ,'X',_ ,_ ,'X',
+               'X','X','Y',_,'X','X','X'
+             ],
+    get3AlignedInDiagonalDownRight(Board, 39, 'X', C1R),
+    assertion(C1R == 1).
+
+test(get3AlignedInDiagonalDownLeft) :-
+    Board = [
+               _,_,_,_,_,_,_,
+               _,_,_,_,_,_,'X',
+               _  ,_  ,  _,  _,  _,  _,'X',
+               'X',_  ,'X',_  ,'X', _ , _ ,
+               _  ,'X',_  ,'X',_  ,_  ,'X',
+               'X','X','Y',_ ,'X','X', 'X'
+             ],
+    get3AlignedInDiagonalDownLeft(Board, 39, 'X', C1R),
+    assertion(C1R == 0).
+
+:- end_tests(triplesDiagonal).
+
+:- begin_tests(triples).
+
+test(countTriples1) :-
+    List = [
+      'X','X','Y','E','E','X','X'
+    ],
+    countTriples(List, 'X', Count),
+    assertion(Count == 0).
+
+test(countTriples2) :-
+    List = [
+      'X','X','Y','Y','X','X','X'
+    ],
+    countTriples(List, 'X', Count),
+    assertion(Count == 0).
+
+test(countTriples3) :-
+    List = [
+      'X','X','X','E','X','X','X'
+    ],
+    countTriples(List, 'X', Count),
+    assertion(Count == 2).
+
+test(countTriples4) :-
+    List = [
+      'E','X','X','X','E','X','X'
+    ],
+    countTriples(List, 'X', Count),
+    assertion(Count == 1).
+
+test(countTriples5) :-
+    List = [
+      'E','X','X','X','Y','X','X'
+    ],
+    countTriples(List, 'X', Count),
+    assertion(Count == 1).
+
+test(countTriples6) :-
+    List = [
+      'X','X','Y','Y','E','E','E'
+    ],
+    countTriples(List, 'X', Count),
+    assertion(Count == 0).
+
+test(countTriples7) :-
+    List = [
+      'X','X','Y','E', 'X','X','X'
+    ],
+    countTriples(List, 'X', Count),
+    assertion(Count == 1).
+
+test(countTriples8) :-
+    List = [
+      'E','E','E','E', 'X','X','X'
+    ],
+    countTriples(List, 'X', Count),
+    assertion(Count == 1).
+
+test(countTriples9) :-
+    List = [
+     'X','X','Y','E','X','X','X'
+    ],
+    countTriples(List, 'X', Count),
+    assertion(Count == 1).
+
+:- end_tests(triples).
+
+:- begin_tests(tuples).
+
+test(countTuples1) :-
+    List = [
+      'X','X','Y','E','E','X','X'
+    ],
+    countTuples(List, 'X', Count),
+    assertion(Count == 1).
+
+test(countTuples2) :-
+    List = [
+      'X','X','Y','Y','X','X','X'
+    ],
+    countTuples(List, 'X', Count),
+    assertion(Count == 0).
+
+test(countTuples3) :-
+    List = [
+      'X','X','E','E','Y','X','X'
+    ],
+    countTuples(List, 'X', Count),
+    assertion(Count == 1).
+
+test(countTuples4) :-
+    List = [
+      'E','X','X','Y','E','X','X'
+    ],
+    countTuples(List, 'X', Count),
+    assertion(Count == 0).
+
+test(countTuples5) :-
+    List = [
+      'E','X','X','Y','Y','X','X'
+    ],
+    countTuples(List, 'X', Count),
+    assertion(Count == 0).
+
+test(countTuples6) :-
+    List = [
+      'X','X','Y','Y','E','E','E'
+    ],
+    countTuples(List, 'X', Count),
+    assertion(Count == 0).
+
+test(countTuples7) :-
+    List = [
+      'X','X','Y','E', 'X','X','E'
+    ],
+    countTuples(List, 'X', Count),
+    assertion(Count == 1).
+
+test(countTuples8) :-
+    List = [
+      'E','E','E','E', 'X','Y','X'
+    ],
+    countTuples(List, 'X', Count),
+    assertion(Count == 0).
+
+test(countTuples9) :-
+    List = [
+     'X','X','E','E','X','X','Y'
+    ],
+    countTuples(List, 'X', Count),
+    assertion(Count == 2).
+
+:- end_tests(tuples).
+
+:- begin_tests(gameOver).
+
+test(gameOver1) :-
+    Board = [
+      '.',_,_,_,_,_,_,
+      _,_,_,_,_,_,'X',
+      _,_,_,_,_,_,'X',
+      'X',_, 'X',_, 'X',_,_,
+      _, 'X',_, 'X',_,_, 'X',
+      'X','X','Y',_, 'X',_, 'X'
+    ],
+    \+ gameOver(Board, 39, _).
+
+test(gameOver2) :-
+     Board = [
+       _,_,_,_,_,_,_,
+       _,_,_,_,_,_,'X',
+       _,_,_,_,_,_,'X',
+       'X',_, 'X',_, 'X',_,_,
+       _, 'X',_, 'X',_,_, 'X',
+       'X','X','Y','X','X','X','X'
+     ],
+     gameOver(Board, 39, _).
+
+
+test(gameOver3) :-
+     Board = [
+       'X','X','X','Y','Y','X','Y',
+       'Y','X','Y','X','Y','Y','X',
+       'X','Y','X','Y','X','X','X',
+       'X','Y','X','Y','X','Y','Y',
+       'Y','X','Y','X','Y','X','X',
+       'X','X','Y','X','X','Y','X'
+     ],
+     gameOver(Board, 39, _).
+
+:- end_tests(gameOver).
+
+:- begin_tests(weight).
+
+test(getWeightPosition1) :-
+    Board = [
+      _,_,_,_,_,_,_,
+      _,_,_,_,_,_,'X',
+      _,_,_,_,_,_,'X',
+      'X',_, 'X',_, 'X',_,_,
+      _, 'X',_, 'X',_,_, 'X',
+      'X','X','Y',_, 'X','X','X'
+    ],
+    getWeightPosition(Board, 39, 'X', Weight),
+    assertion(Weight == 200).
+
+test(getWeightPosition2) :-
+    Board = [
+      _,_,_,_,_,_,_,
+      _,_,_,_,_,_,'X',
+      _,_,_,_,_,_,'X',
+      'X',_  ,_  ,_  ,'X',_  ,_  ,
+      _  ,'X',_  ,'X',_  ,'X','X',
+      'X','X','Y',_  ,'X','X',_
+    ],
+    getWeightPosition(Board, 39, 'X', Weight),
+    assertion(Weight == 20).
+
+:- end_tests(weight).
+
     Board = 42,
     assertion(Board == 42).
 
