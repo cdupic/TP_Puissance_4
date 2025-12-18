3e889ebc4eaa426337828f2d821519319bfe4bf9
aurelianfrg <aurelianfrangin@gmail.com>
2025-12-18 14:08:51 +0100

    updated readme

--- src/ia/neural-network-ReLu-Aurelian.pl
+++ src/ia/neural-network-ReLu-Aurelian.pl
@@ -3,7 +3,7 @@
 neuralNetworkAiReLu(Board, Move, Player) :-
     formatBoardForNN(Board, Player, Str),
     %writeln(Str),
-    format(string(Command), "C:\\Users\\aurel\\Desktop\\Cours\\4A\\ALIA\\TP_Puissance_4\\src\\Neural-Network\\move_calculation-ReLu.exe ~w", [Str]),
+    format(string(Command), ".\\move_calculation-ReLu.exe ~w", [Str]),
     %writeln(Command),
     shell(Command, Column),
     %writeln(Column),
